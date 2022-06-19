3daec1b59512d6d9a46098163a60b5809f5da353
tru10b <true-ten@yandex.ru>
2022-06-19 22:55:30 +0300

    first commit

--- src/main/java/second_exercise_main.java
+++ src/main/java/second_exercise_main.java
@@ -1,4 +1,4 @@
-public class main {
+public class second_exercise_main {
     public static void main(String[] args) {
         System.out.println(daysToMsecFromNow(-10L));
         System.out.println(daysToMsecFromNow(2000L));
