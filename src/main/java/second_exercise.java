59c0560f6a78c6ecbb5ab021026916fa5f497f8b
tru10b <true-ten@yandex.ru>
2022-06-20 21:24:31 +0300

    fourth commit

--- src/main/java/second_exercise.java
+++ src/main/java/second_exercise.java
@@ -1,24 +1,27 @@
 public class second_exercise {
     public static void main(String[] args) {
-        System.out.println(daysToMsecFromNow(-10L));
-        System.out.println(daysToMsecFromNow(Long.MAX_VALUE));
+
+        System.out.println(daysToMsecFromNow(-10));
+        System.out.println(daysToMsecFromNow(Integer.MAX_VALUE));
+
         System.out.println(addDepositUpd(100000, 10000000));
         System.out.println(addDepositUpd(Integer.MAX_VALUE, 1));
         System.out.println(addDepositUpd(Integer.MIN_VALUE, -20));
     }
 
-    private static int addDepositUpd(int deposit, int income) {
-        boolean isOwerflow = true;
-        if (Math.abs((long)deposit+(long)income)>Integer.MAX_VALUE) isOwerflow = false;
-        return isOwerflow ? (deposit + income) : 0;
+    private static long daysToMsecFromNow(int days) {
+        long timeInMS = System.currentTimeMillis() + days * 24L * 60L * 60L * 1000L;
+        return timeInMS;
     }
 
-    private static long daysToMsecFromNow(long days) {
-        long timeInMS = System.currentTimeMillis() + days * 24L * 60L * 60L * 1000L;
-        long overFlawCrit=(Long.MAX_VALUE-System.currentTimeMillis())/(24L * 60L * 60L * 1000L);
-        return (days>overFlawCrit)? 0:timeInMS;
-        }
+    private static int addDepositUpd(int deposit, int income) {
+        boolean isOwerflow = true;
+        long sum= (long) deposit + (long) income;
+        if ((sum>Integer.MAX_VALUE) || (sum < Integer.MIN_VALUE)) isOwerflow = false;
+        return isOwerflow ? (deposit + income) : 0;
     }
+}
+
 
 
 
