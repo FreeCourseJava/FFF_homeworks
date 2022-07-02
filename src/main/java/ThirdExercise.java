f9a9389b5760b3f6b2588e4cbca12ce9cda8f58f
tru10b <true-ten@yandex.ru>
2022-07-03 00:54:46 +0300

    second commit

--- src/main/java/ThirdExercise.java
+++ src/main/java/ThirdExercise.java
@@ -5,27 +5,31 @@ public class ThirdExercise {
 
         int[] TatarstanYears = {1992, 1920, 2006, 1990, 1438, 1236, 1458, 1917, 1552, 1708,
                 1445, 1723, 1718, 1551, 1752, 965, 1380, 1891, 1970, 1937};
+        int[] TatarstanYears2 = {1801, 1851, 1876, 1886, 1896, 1901, 1906, 1907, 1911, 1913,
+                1916, 1921, 1926, 1931, 1936, 1956, 1961, 1966, 1976, 1996};
         int[] sortedTatarstanYears = bubbleSort(TatarstanYears);
         for (int i = 0; i < 20; i++) {
             System.out.println(sortedTatarstanYears[i]);
         }
         System.out.println(binarySearch(sortedTatarstanYears, 1718));
         System.out.println(binarySearch(sortedTatarstanYears, 965));
-        System.out.println(binarySearch(sortedTatarstanYears, 2022));
+        System.out.println(binarySearch(sortedTatarstanYears, 1380));
+        System.out.println(binarySearch(sortedTatarstanYears, 1915));
+        System.out.println(binarySearch(TatarstanYears2, 1852));
     }
 
     private static int arrayChangesToCount(int size) {
-        int[] massiv = new int[size];
+        int[] array = new int[size];
         int counter = 0;
         for (int i = 1; i <= size; i++) {
             if (size % i == 0) {
-                massiv[i - 1] = i;
+                array[i - 1] = i;
             } else {
-                massiv[i - 1] = Integer.MIN_VALUE;
+                array[i - 1] = Integer.MIN_VALUE;
             }
         }
         for (int i = 1; i <= size; i++) {
-            if (massiv[i - 1] != Integer.MIN_VALUE) {
+            if (array[i - 1] != Integer.MIN_VALUE) {
                 counter++;
             }
         }
@@ -33,40 +37,42 @@ public class ThirdExercise {
     }
 
 
-    private static int[] bubbleSort(int[] massiv) {
+    private static int[] bubbleSort(int[] array) {
         int swap;
-        for (int j = 0; j < massiv.length; j++) {
-            for (int i = 0; i < (massiv.length - j - 1); i++) {
-                if (massiv[i] > massiv[i + 1]) {
-                    swap = massiv[i + 1];
-                    massiv[i + 1] = massiv[i];
-                    massiv[i] = swap;
+        for (int j = 0; j < (array.length - 1); j++) {
+            for (int i = 0; i < (array.length - j - 1); i++) {
+                if (array[i] > array[i + 1]) {
+                    swap = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = swap;
                 }
             }
         }
-        return massiv;
+        return array;
     }
 
-    public static boolean binarySearch(int[] massiv, int value) {
-        int key = (massiv.length - 1) / 2;
+    public static boolean binarySearch(int[] array, int value) {
+        int left = 0;
+        int right = array.length - 1;
+        int key;
         for (; ; ) {
-
-            if (massiv[key] > value) {
-                key = (key - 1) / 2;
-            } else if (massiv[key] < value) {
-                key = (key + massiv.length) / 2;
-            } else if (massiv[key] == value) {
+            key = (left + right) / 2;
+            if (array[key] > value) {
+                right = key - 1;
+            } else if (array[key] < value) {
+                left = key + 1;
+            } else {
                 return true;
             }
 
-            if (key == 0 || key == (massiv.length - 1)) {
-                if (massiv[key] == value) {
-                    return true;
-                }
+            if (left > right) {
                 return false;
             }
         }
     }
 }
+
+
+
 
 
